9034a8ad91ce3c32c71882c7bb9df6bb60725490
jsouthard-fcc <jsouthard@frederick.edu>
2026-02-10 13:19:18 -0500

    Changed greeting to p.m.

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Good morning, CMSC 131!");
+        System.out.println("Good afternoon, CMSC 131!");
         System.out.println("How are you today?");
         System.out.println("This line was typed in IntelliJ");
     }
